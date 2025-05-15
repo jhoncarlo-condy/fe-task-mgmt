156f73a6bf298ce1780a66c055e65266d7a3ae1c
condy <jhoncarlo.condy@gmail.com>
2025-05-15 22:01:46 +0800

    initial commit for fe-task-mgmt

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { InteractiveGridPattern } from '@/components/ui/interactive-grid-pattern'
+import LandingView from './views/LandingView.vue'
+</script>
+
+<template>
+  <div class="relative grid h-[1000px] w-full place-content-center overflow-hidden">
+    <LandingView />
+    <InteractiveGridPattern
+      :class="'[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]'"
+      :width="20"
+      :height="20"
+      :squares="[80, 80]"
+      squares-class-name="hover:fill-blue-500"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/LandingView.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+</script>
+<template>
+  <div class="z-10">
+    <p
+      class="whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white"
+    >
+      Task Manager
+    </p>
+    <div class="flex flex-row items-center text-center justify-center p-10 gap-5">
+      <Dialog>
+        <DialogTrigger as-child>
+          <Button variant="default" class="hover:cursor-pointer"> Start </Button>
+        </DialogTrigger>
+        <DialogContent class="w-full">
+          <Tabs default-value="account" class="w-full p-2">
+            <TabsList class="grid w-full grid-cols-2">
+              <TabsTrigger value="account" class="hover:cursor-pointer"> Login </TabsTrigger>
+              <TabsTrigger value="register" class="hover:cursor-pointer"> Register </TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">
+              <Card>
+                <CardContent class="space-y-4">
+                  <div class="space-y-1">
+                    <Label for="email">Email</Label>
+                    <Input id="email" type="email" />
+                  </div>
+                  <div class="space-y-1">
+                    <Label for="password">Password</Label>
+                    <Input id="password" type="password" />
+                  </div>
+                  <div class="space-y-4 place-self-end">
+                    <Button class="hover:cursor-pointer">Login</Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="register">
+              <Card>
+                <CardContent class="space-y-4">
+                  <div class="space-y-1">
+                    <Label for="firstName">First Name</Label>
+                    <Input id="firstName" type="text" />
+                  </div>
+                  <div class="space-y-1">
+                    <Label for="lastName">Last Name</Label>
+                    <Input id="lastName" type="text" />
+                  </div>
+                  <div class="space-y-1">
+                    <Label for="email">Email</Label>
+                    <Input id="email" type="email" />
+                  </div>
+                  <div class="space-y-1">
+                    <Label for="password">Password</Label>
+                    <Input id="password" type="password" />
+                  </div>
+                  <div class="space-y-1">
+                    <Label for="confirm_password">Confirm Password</Label>
+                    <Input id="confirm_password" type="confirm_password" />
+                  </div>
+                  <div class="space-y-4 place-self-end">
+                    <Button class="hover:cursor-pointer">Register</Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </DialogContent>
+      </Dialog>
+    </div>
+  </div>
+</template>
+
+<style></style>
